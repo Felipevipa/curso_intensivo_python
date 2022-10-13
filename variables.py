cb7643406e7e058b5285f361746c054204d0dcc7
unknown <felipevipa@outlook.com>
2022-10-13 11:25:59 -0500

    variables y tipos de variables

---
+++ variables.py
@@ -0,0 +1,60 @@
+print("Hola")
+
+mensaje = "Hola mensaje"
+print(mensaje)
+
+# snake_case
+mi_primer_variable = "Hola, ¿como estas?"
+VELOCIDAD_DEL_SONIDO_EN_0_GRADOS = 331
+
+# Asignaciones
+variable1 = 4
+variable1, variable2 = 4, 8
+variable3 = variable2
+print(variable3)
+
+## tipos de datos
+# Booleanos, solo admite dos valores, que son True y False
+
+variable_booleana1 = True
+variable_booleana2 = False
+print(type(variable_booleana1))
+
+# Enteros, admiten todos los números enteros pero que no requieran decimales
+variable_entera1 = 1
+variable_entera2 = -6
+print(type(variable_entera1))
+
+
+# Flotantes, admite todos los valores que requieran amrcas decimales. Pero tambien consume mayor memoria
+variable_flotante1 = 1.0
+variable_flotante2 = 6.7
+print(type(variable_flotante1))
+
+
+# Complejos, admite numeros reales e imaginarios
+variable_compleja1 = 1.0 + 6.0j
+variable_compleja2 = 6.7 + 3.2j
+print(type(variable_compleja1))
+
+# Cadenas de caracteres o texto
+variable_cadena1 = "Cadena de texto 1"
+variable_cadena2 = "Cadena de texto 2"
+print(type(variable_cadena1))
+
+
+# Listas, es una colección de datos.
+variable_lista1 = [1, 2, 3]
+variable_lista2 = [6,"Cadena de texto", 3.2]
+print(type(variable_lista1))
+
+# Diccionarios, tambien una colección de datos, pero con la diferencia que los datos se almacenan en pares, clave y valor.
+variable_diccionario1 = {"valor1": 1,
+                         "valor2": 2,
+                         "valor3": 3}
+
+variable_diccionario2 = {"nombre": "Maria",
+                         "edad": 15,
+                         "genero": "femenino"}
+
+print(type(variable_diccionario1))
